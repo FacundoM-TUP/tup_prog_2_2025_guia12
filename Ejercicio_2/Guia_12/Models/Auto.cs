3ff63e66686d3e5b3ff5e309427b11c370e0a862
FacundoM-TUP <martinezfacundo942@gmail.com>
2025-10-30 18:05:52 -0300

    completar

---
+++ Ejercicio_2/Guia_12/Models/Auto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2.Models
+{
+    public class Auto
+    {
+        private int numeroregistro;
+        private string modelo;
+        public double precio {  get; set; }
+        public Auto(int registro, string modelo)
+        {
+            this.numeroregistro = registro;
+            this.modelo = modelo;
+        }
+        public int nroregistro()
+        {
+            return numeroregistro;
+        }
+        public override string ToString()
+        {
+            return $"{numeroregistro};{modelo}";
+        }
+    }
+}
---
+++ Ejercicio_2/Guia_12/Models/Camion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2.Models
+{
+    public class Camion
+    {
+        private Stack<Auto> transporte = new Stack<Auto>();
+        private DateTime fecha;
+        private int cantidad;
+        private double valorasegurado;
+        public int NroRegistro { get; set; }
+        public Camion(DateTime fecha, int cantidad)
+        {
+            this.fecha = fecha;
+            this.cantidad = cantidad;
+        }
+        public void cargarvehiculo(Auto unauto)
+        {
+            if (transporte.Count < cantidad)
+            {
+                transporte.Push(unauto);
+            }
+        }
+        public Auto retirarvehiculo()
+        {
+            if (transporte.Count > 0)
+            {
+                return eliminado = transporte.Pop();
+            }
+            return null;
+        }
+    }
+}
